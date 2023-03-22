b4e5b1e0a764991b90ca19b41565d53f31ac9c96
juanandresMedia <juanbedoyacardenas@iesantaelena.edu.co>
2023-03-22 16:29:20 -0500

    hola

--- Script/script.js
+++ Script/script.js
@@ -1,11 +1,38 @@
-nombre, correo, mensaje;
-let formulario = document.getElementById(`form`)
-formulario.addEventListener(`submit`, (e)=>{
+let nombre, correo, mensaje;
+let formulario = document.getElementById("form")
+formulario.addEventListener("submit", (e) => {
     e.preventDefault()
-    leerDatos()
+    LeerDatos()
 })
-function leerDatos(){
-    nombre = document. getElementById(`nombre`).value
-correo = document.getElementById(`correo`).value
-mensaje = document.getElementById(`textarea`).value
+
+function LeerDatos() {
+    nombre = document.getElementById("nombre").value
+    correo = document.getElementById("correo").value
+    mensaje = document.getElementById("textarea").value
+    ValidarData(nombre, correo, mensaje)
+    GuardarLocalStorage(nombre,correo,mensaje)
+}
+function ValidarData(nombre, correo, mensaje)
+ 
+{
+    if (nombre.length == 0 || correo.length == 0 || mensaje.length == 0) {
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Complete todos los campos',
+            footer: '<a href="">Why do I have this issue?</a>'
+        })
+    }
+}
+function GuardarLocalStorage(nombre,correo,mensaje) {
+    localStorage.setItem("Nombre",nombre)
+    localStorage.setItem("Correo",correo)
+    localStorage.setItem("Mensaje",mensaje)
+    ListarData()
+}
+function ListarData() {
+    let nombreUsu = localStorage.getItem("Nombre")
+    let correoUsu = localStorage.getItem("Correo")
+    let mensajeUsu = localStorage.getItem("Correo")
 }
